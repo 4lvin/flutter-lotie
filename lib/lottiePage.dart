3b1bcf7007a984f7bb6bd940426e99e93badbcb0
Alvin <phienal181@gmail.com>
2020-06-14 14:57:52 +0700

    first commit

---
+++ lib/lottiePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LottiePage extends StatefulWidget {
+  @override
+  _LottiePageState createState() => _LottiePageState();
+}
+
+class _LottiePageState extends State<LottiePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/masker.json',
+          width: 200,
+          height: 200,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
